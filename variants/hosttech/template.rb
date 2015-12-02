be000a785b629a8f4d93f1e9b8c37cb05296cb31
Emanuel Imhof <hello@m43nu.ch>
2015-12-02 10:02:16 +0100

    overwrite nginx.rake to enable the site within sites-available; remove default and deferred from listen-directive; use the domain name as subdomain before the server

--- variants/hosttech/template.rb
+++ variants/hosttech/template.rb
@@ -2,4 +2,5 @@ source_paths.unshift(File.dirname(__FILE__))
 
 copy_file "lib/capistrano/mb/templates/nginx_unicorn.erb", force: true
 copy_file "lib/capistrano/tasks/hosting.rake", force: true
+copy_file "lib/capistrano/mb/tasks/nginx.rake", force: true
 apply "config/deploy.rb"
