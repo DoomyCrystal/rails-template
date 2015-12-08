e6de2bbd4dfb2451cc5a99af9ed873a804d909f5
Emanuel Imhof <hello@m43nu.ch>
2015-12-08 08:36:12 +0100

    add corect file-ending to maintenance.html

--- lib/template.rb
+++ lib/template.rb
@@ -1,7 +1,7 @@
 copy_file "lib/capistrano/tasks/newrelic.rake"
 copy_file "lib/capistrano/mb/templates/newrelic.yml.erb"
 copy_file "lib/capistrano/mb/templates/crontab.erb"
-copy_file "lib/capistrano/mb/templates/maintenance.erb.html"
+copy_file "lib/capistrano/mb/templates/maintenance.html.erb"
 copy_file "lib/tasks/assets.rake"
 copy_file "lib/tasks/auto_annotate_models.rake"
 copy_file "lib/tasks/coverage.rake"
