4990ef5214f9831e0a31c30cf6c9ae0e982f7c78
Emanuel Imhof <hello@m43nu.ch>
2016-06-27 10:12:16 +0200

    fixing a typo in oriented.net-variant, breaking the script when copying the Gemfile-entries

--- lib/template.rb
+++ lib/template.rb
@@ -1,4 +1,5 @@
 copy_file "lib/capistrano/tasks/newrelic.rake"
+copy_file "lib/capistrano/tasks/maintenance.rake"
 copy_file "lib/capistrano/mb/templates/newrelic.yml.erb"
 copy_file "lib/capistrano/mb/templates/crontab.erb"
 copy_file "lib/capistrano/mb/templates/maintenance.html.erb"
--- variants/oriented/template.rb
+++ variants/oriented/template.rb
@@ -1,7 +1,7 @@
 source_paths.unshift(File.dirname(__FILE__))
 
+apply 'Gemfile.rb'
 apply 'config/deploy.rb'
 apply 'Capfile.rb'
-apply 'Gemfile'
 template 'config/deploy/production.rb.tt', force: true
 template 'config/deploy/staging.rb.tt', force: true
