53cc74b5897f6a3713c9a2002d6bf9a60ea6c7a3
Emanuel Imhof <hello@m43nu.ch>
2016-08-15 13:42:21 +0200

    update alchemy_cms to version 3.4

--- variants/alchemycms/Gemfile.rb
+++ variants/alchemycms/Gemfile.rb
@@ -1,6 +1,6 @@
 insert_into_file "Gemfile", :after => /gem "bcrypt".*\n/ do
   <<-GEMS.strip_heredoc
-    gem 'alchemy_cms', '~> 3.3.0'
-    gem 'alchemy-devise', github: 'AlchemyCMS/alchemy-devise', tag: 'v3.3.0'
+    gem 'alchemy_cms', '~> 3.4.0'
+    gem 'alchemy-devise', github: 'AlchemyCMS/alchemy-devise', branch: '3.4-stable'
   GEMS
 end
