08b2008ea0a3071066a5f4903996a92abc6723dd
Emanuel Imhof <hello@m43nu.ch>
2016-09-02 14:57:30 +0200

    Extended bootstrap-variant.
    * updated gem version to 3.3.7
    * add a partial all_elements, containing all ui-elements within bootstrap to simplify styling

--- variants/bootstrap/template.rb
+++ variants/bootstrap/template.rb
@@ -11,6 +11,7 @@ template "app/views/layouts/application.html.haml.tt", :force => true
 template "app/views/shared/_footer.html.haml.tt"
 template "app/views/shared/_navbar.html.haml.tt"
 copy_file "app/views/shared/_page_header.haml"
+copy_file "app/views/shared/_all_elements.html.haml"
 copy_file "lib/templates/haml/controller/view.html.haml"
 copy_file "lib/templates/haml/scaffold/_form.html.haml"
 copy_file "lib/templates/haml/scaffold/edit.html.haml"
