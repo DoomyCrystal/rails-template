065bb854662e328471bff00a427694b3cff4cc66
Emanuel Imhof <hello@m43nu.ch>
2020-08-24 13:59:46 +0200

    enhancement - use alchemy_cms/devise stable-branch instead of release from rubygem

--- variants/alchemycms/Gemfile.rb
+++ variants/alchemycms/Gemfile.rb
@@ -1,7 +1,7 @@
 insert_into_file 'Gemfile', before: /gem 'bootsnap'.*\n/ do
   <<-GEMS.strip_heredoc
-    gem 'alchemy_cms', '~> 5.0.0'
-    gem 'alchemy-devise', '~> 4.5.0'
-    gem 'alchemy-richmedia-essences', '~> 2.0.0'
+    gem 'alchemy_cms', git: 'https://github.com/AlchemyCMS/alchemy_cms.git', branch: '5.0-stable'
+    gem 'alchemy-devise', git: 'https://github.com/AlchemyCMS/alchemy-devise.git', branch: '5.0-stable'
+    gem 'alchemy-richmedia-essences', '~> 2.3.0'
   GEMS
 end
