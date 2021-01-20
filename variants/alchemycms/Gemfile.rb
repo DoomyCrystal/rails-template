ba1ab36b11718f68f2e16c3c550d14be627dc7e4
Emanuel Imhof <hello@m43nu.ch>
2021-01-20 10:19:33 +0100

    enhancement - update alchemy_cms to 5.1.1 and alchemy-devise to 5.1

--- variants/alchemycms/Gemfile.rb
+++ variants/alchemycms/Gemfile.rb
@@ -1,7 +1,7 @@
 insert_into_file 'Gemfile', before: /gem 'bootsnap'.*\n/ do
   <<-GEMS.strip_heredoc
-    gem 'alchemy_cms', git: 'https://github.com/AlchemyCMS/alchemy_cms.git', branch: '5.0-stable'
-    gem 'alchemy-devise', git: 'https://github.com/AlchemyCMS/alchemy-devise.git', branch: '5.0-stable'
+    gem 'alchemy_cms', '~> 5.1.1'
+    gem 'alchemy-devise', '~> 5.1.0'
     gem 'alchemy-richmedia-essences', '~> 2.3.0'
   GEMS
 end
