fe61a4f9226de3a526af751c0916324deca091df
Oliver Meyer <34474012+DoomyCrystal@users.noreply.github.com>
2021-08-11 13:56:39 +0200

    bugfix - downgrade gutentag to fix migrations

--- variants/alchemycms/Gemfile.rb
+++ variants/alchemycms/Gemfile.rb
@@ -2,5 +2,6 @@ insert_into_file 'Gemfile', before: /gem 'bootsnap'.*\n/ do
   <<-GEMS.strip_heredoc
     gem 'alchemy_cms', '~> 6.0.0.b1'
     gem 'alchemy-devise', '~> 5.1.0'
+    gem 'gutentag', '2.5.4'
   GEMS
 end
